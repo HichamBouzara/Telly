026b84b848aa8ae4934c5eba70d2511bb470f855
Hichem Bouzara <fa_bouzara@esi.dz>
2018-11-11 13:54:47 +0100

    Added Logout functionality

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -21,7 +21,13 @@ public class UserController {
 	@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
+    }
+    
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
 	}
+	
 	
 
 }
